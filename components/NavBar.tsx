51fc20149bd8a2d406da9bc9cc8ab7ab9e7ef524
Florian <mrminecrafttv19@gmail.com>
2024-08-04 13:42:17 +0200

    chore: Some css changes

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <div className='bg-dark-100 p-2'>
         <div className='pl-2 pr-2'>
-            <p className='font-spaceGrotesk text-white text-[2rem] font-bold'>Fort<span className='text-primary-dark'>Code</span></p>
+            <p className='font-spaceGrotesk text-white text-[2rem] font-bold glow'>Fort<span className='text-primary-dark blue-glow'>Code</span></p>
         </div>
     </div>
   )
--- components/form/Header.tsx
+++ components/form/Header.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header = () => (
     <div className='flex flex-col items-center justify-center text-center'>
-        <h1 className='text-white text-[2.5rem] glow font-bold'>FortCode</h1>
-        <p className='text-white text-[1.25rem]'>Make your text a bit more.. Fortnite.</p>
+        <h1 className='text-white text-[2.5rem] font-bold'>FortCode</h1>
+        <p className='text-white text-[1.25rem]'>Make your text a bit more.. fortnite.</p>
     </div>
 );
 
