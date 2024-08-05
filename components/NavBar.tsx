69e90dccaeacd431fcaa6a78bc9c51443900549e
Florian <mrminecrafttv19@gmail.com>
2024-08-05 22:23:51 +0200

    feat: added link into the navbar
    User can go to the home page with a click via the navbar text

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,8 +5,11 @@ const NavBar = () => {
   return (
     <div className='bg-dark-100 p-4'>
         <div className='pl-2 pr-2 flex'>
-            <p className='font-spaceGrotesk text-white text-[2rem] font-bold glow'>Fort<span className='text-primary-dark blue-glow'>Code</span></p>
-            <div className={'flex-grow'} />
+            <Link href={'/'}>
+                <p className='font-spaceGrotesk text-white text-[2rem] font-bold glow'>Fort<span
+                    className='text-primary-dark blue-glow'>Code</span></p>
+            </Link>
+            <div className={'flex-grow'}/>
             <Link href={'/docs'}>
                 <p className={'font-spaceGrotesk text-primary-dark text-[2rem] font-bold'}>API</p>
             </Link>
